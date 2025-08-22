0d945aee0dfc090141fdba98fdf256c10855b0fa
SarAvi21805 <alejandraviles2005@gmail.com>
2025-08-21 20:32:51 -0600

    Última modificación para pull request.

--- app/src/main/java/com/example/laboratorio5/MainActivity.kt
+++ app/src/main/java/com/example/laboratorio5/MainActivity.kt
@@ -1,6 +1,7 @@
 /* Alejandra Avilés - 24722
 *  Laboratorio 4.2
-*  Programación de Plataformas Móviles */
+*  Programación de Plataformas Móviles
+* Fecha de entrega : 21/08/2025 */
 
 package com.example.laboratorio5
 
